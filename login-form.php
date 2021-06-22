0eb57eeb01a37a96b4cc1bb52cec91afc97e6b20
Jen Gray <69304519+jgray13@users.noreply.github.com>
2021-06-22 20:58:28 +0100

    Add files via upload

--- login-form.php
+++ login-form.php
@@ -1,18 +1,54 @@
 <?php
+session_start();
 $page_title = 'Login';
+$username = '';
+$password = '';
+$username_error = '';
+$password_error = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if (isset($_POST['username'])) {
+		$username = trim($_POST['username']);
+		if (!ctype_alnum($username)) {
+			$username_error = 'Invalid username';
+		}
+	}
+	if (empty ($_POST['username'])) {
+		$username_error = 'Please enter a username';
+	}
+	if (isset($_POST['password'])) {
+		$password = trim($_POST['password']);
+		if (!ctype_alnum($password)) {
+			$password_error = 'Invalid password';
+		}
+	}
+	if (empty($_POST['password'])) {
+		$password_error = 'Please enter a password';
+	} else {
+		$_SESSION['username'] = $username;
+		$_SESSION['password'] = $password;
+	    header('Location: welcome.php');	
+	}
+}
 include 'includes/header.html';
 ?>
-		    <form action="login-handler.php" method="POST">
-			<p>
-			Please enter alphanumeric characters
-			</p>
-			<div id="form-inner">
-			    <label for="name">Username: </label>
-				<input type="text" name="username" id="username"><br>
-				<label for="password">Password: </label>
-				<input type="password" name="password" id="password"><br>
-		        <input type="submit" name="submit" value="Submit" class="submit-button">
-		    </div>
+		    <form action="" method="POST">
+			    <h1>Login</h1>
+			    <div id="form-inner">
+			        <fieldset>
+			            <legend>Please enter alphanumeric characters</legend>
+			            <label for="name">Username: </label>
+				        <input type="text" name="username" id="username" value="<?php if (isset($_POST['username'])) echo $_POST['username'];?>">
+				        <p class="error-message"><?php if (!empty($username_error)) echo $username_error;?></p>
+				
+				        <br><label for="password">Password: </label>
+				        <input type="password" name="password" id="password">
+				        <p class="error-message"><?php if (!empty($password_error)) echo $password_error;?></p>
+				
+		                <br><input type="submit" name="submit" value="Submit" class="submit-button">
+		    
+			        </fieldset>
+			    </div>
+			</form>
 <?php
 include 'includes/footer.html';
 ?>
--- welcome.php
+++ welcome.php
@@ -1,8 +1,15 @@
 <?php
+session_start();
 $page_title = 'Welcome';
 include 'includes/header.html';
+if (isset($_SESSION['username'])) {
+	$username = $_SESSION['username'];
+	echo "<h1>Welcome!</h1><br><p>Hi $username!<p>";
+} else {
+	echo 'Please <a href="login-form.php">Log in</a>';
+}
 ?>
-<h1>Welcome!</h1>
+<a href="logout.php">Logout</a>
 
 <?php
 include 'includes/footer.html';
